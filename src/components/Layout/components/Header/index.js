530184b91924994f64d74092d73721c414889a51
KaiO277 <nghiatran1527@gmail.com>
2024-10-18 22:16:47 +0700

    KaiO build Header p2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,24 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images/logo.png';
 
 const cx = classNames.bind(styles);
+// console.log(images);
 
 function Header() {
-    return <header className={cx('wrapper')}></header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images} alt="Logo" />
+                </div>
+                <div className={cx('name-app')}>
+                    <p>Hope Horizon</p>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
